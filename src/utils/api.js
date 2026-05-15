b5b217d912e01216fb0e9fc83e2b34212713f00f
Shweta Maurya <shwetamaurya327@gmail.com>
2026-05-16 00:32:13 +0530

    fix it

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:5000/api"
-  baseURL:"https://smart-appointment-system-3ahc.onrender.com"
+  baseURL:"https://smart-appointment-system-3ahc.onrender.com/api"
 });
 
 export default api;
