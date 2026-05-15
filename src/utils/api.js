e29e367f97a90c913787412446f01311beca8640
Shweta Maurya <shwetamaurya327@gmail.com>
2026-05-16 00:06:00 +0530

    fix base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  //baseURL: "http://localhost:5000/api"
+  baseURL:"https://smart-appointment-system-3ahc.onrender.com"
 });
 
 export default api;
